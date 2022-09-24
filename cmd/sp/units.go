06149de737d8ac7b0eb36fbfb73aa45105abe15d
Rick Harris <rconradharris@gmail.com>
2022-09-24 16:03:59 -0500

    Add TemperatureUnit to units pkg

--- cmd/sp/units.go
+++ cmd/sp/units.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/rconradharris/go-sensorpush/units"
@@ -21,52 +20,23 @@ func addUnitFlags(fs *flag.FlagSet, f *unitFlags) {
 	fs.StringVar(&f.temp, "temp", "f", "fahrenheit (\"f\") or celsius (\"c\")")
 }
 
+type unitsFormatter struct {
+	tempU units.TemperatureUnit
+}
+
+
 func newUnitsFormatter(uf *unitFlags) (*unitsFormatter, error) {
-	cfg := unitsCfg{}
+	fmtU := &unitsFormatter{}
 
 	if uf != nil {
-		tempU, err := newTemperatureUnit(uf.temp)
+		tempU, err := units.ParseTemperatureUnit(uf.temp)
 		if err != nil {
 			return nil, err
 		}
-		cfg.temperature = tempU
+		fmtU.tempU = tempU
 	}
 
-	return &unitsFormatter{cfg: cfg}, nil
-}
-
-type temperatureUnit int
-
-func newTemperatureUnit(s string) (temperatureUnit, error) {
-	switch s {
-	case "f":
-		return tempFahrenheit, nil
-	case "c":
-		return tempCelsius, nil
-	}
-	var t0 temperatureUnit
-	return t0, fmt.Errorf("unknown temp unit specifier: %s", s)
-}
-
-func (u temperatureUnit) String() string {
-	switch u {
-	case tempCelsius:
-		return "c"
-	}
-	return "f"
-}
-
-const (
-	tempFahrenheit temperatureUnit = iota
-	tempCelsius
-)
-
-type unitsCfg struct {
-	temperature temperatureUnit
-}
-
-type unitsFormatter struct {
-	cfg unitsCfg
+	return fmtU, nil
 }
 
 func (f *unitsFormatter) Distance(d *units.Distance) string {
@@ -84,32 +54,33 @@ func (f *unitsFormatter) Temperature(t *units.Temperature) string {
 		return notAvail
 	}
 	var v float32
-	switch f.cfg.temperature {
-	case tempCelsius:
+	switch f.tempU {
+	case units.TemperatureUnitC:
 		v = t.C()
 	default:
 		v = t.F()
 	}
 
-	deg := strings.ToUpper(f.cfg.temperature.String())
-	return fmt.Sprintf("%.1f°%s", v, deg)
+	unit := f.tempU.String()
+	return fmt.Sprintf("%.1f°%s", v, unit)
 }
 
 func (f *unitsFormatter) TemperatureDelta(t units.TemperatureDelta) string {
 	var v float32
-	switch f.cfg.temperature {
-	case tempFahrenheit:
-		v = t.F()
-	default:
+	switch f.tempU {
+	case units.TemperatureUnitC:
 		v = t.C()
+	default:
+		v = t.F()
 	}
 
 	sign := "+"
 	if v < 0 {
 		sign = "-"
 	}
-	deg := strings.ToUpper(f.cfg.temperature.String())
-	return fmt.Sprintf("%s%.1f°%s", sign, v, deg)
+
+	unit := f.tempU.String()
+	return fmt.Sprintf("%s%.1f°%s", sign, v, unit)
 }
 
 func (f *unitsFormatter) Humidity(h *units.Humidity) string {
---
+++ units/temperature_unit.go
@@ -0,0 +1,31 @@
+package units
+
+import (
+	"fmt"
+)
+
+type TemperatureUnit int
+
+const (
+	TemperatureUnitF TemperatureUnit = iota
+	TemperatureUnitC
+)
+
+func ParseTemperatureUnit(s string) (TemperatureUnit, error) {
+	var t0 TemperatureUnit
+	switch s {
+	case "f":
+		return TemperatureUnitF, nil
+	case "c":
+		return TemperatureUnitC, nil
+	}
+	return t0, fmt.Errorf("unknown temp unit specifier: %s", s)
+}
+
+func (u TemperatureUnit) String() string {
+	switch u {
+	case TemperatureUnitC:
+		return "C"
+	}
+	return "F"
+}
